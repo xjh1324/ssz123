464d4c15f975be45c58b292412720dd0ad975309
xjh1324 <88362916+xjh1324@users.noreply.github.com>
2021-10-12 11:31:03 +0800

    Add files via upload

--- shengri/js/indexl.js
+++ shengri/js/indexl.js
@@ -25,7 +25,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('Hi 小可爱们|祝你|生日快乐|happy birthday|#countdown 3||');
+      S.UI.simulate('Hi 我的臭宝宝|祝你|生日快乐|happy birthday|#countdown 3||');
     }
 
     S.Drawing.loop(function () {
